9fa4a7f8303d39101add6259bc9c89720439d257
Mferik <mfatiherikk@gmail.com>
2023-07-05 11:29:01 +0300

    DataAccess katmanında, ürünleri getirme,ekleme,silme,güncelleme işlemleri

--- DataAccess/Concrete/InMemory/InMemoryProductDAL.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDAL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,47 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.InMemory
 {
-    public class InMemoryProductDAL
+    public class InMemoryProductDAL : IProductDAL
     {
+        List<Product> _products; //global değişken _ ile kullanılır
+        public InMemoryProductDAL()
+        {
+            //Fake veriler
+            _products = new List<Product>
+            {
+                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15 },
+                new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3 },
+                new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2 },
+                new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65 },
+                new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitsInStock=1 },
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId == product.ProductId); //LINQ //ID bazlı taramalarda SingleOrDefault kullanabilirim.
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public void Update(Product product)
+        {
+            //Güncellenecek referansı buluyorum. Gönderdiğim ürün id'sine sahip ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId); 
+           //Güncelleme işlemleri
+            productToUpdate.ProductName = product.ProductName;         
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+
+        }
     }
 }
