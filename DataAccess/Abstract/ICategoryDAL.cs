987ad57402aec1ddd32f0fd290e283fd1b87bf6d
Mferik <mfatiherikk@gmail.com>
2023-07-05 22:38:59 +0300

    IEntityRepository oluşturuldu. Customer sınıfı eklendi

--- DataAccess/Abstract/ICategoryDAL.cs
+++ DataAccess/Abstract/ICategoryDAL.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDAL : IEntityRepository<Product>
+    public interface ICategoryDAL :IEntityRepository<Category>
     {
-
+       
     }
 }
