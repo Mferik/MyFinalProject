ab5334b0a8c11dd9779cd5f6d81fa37eb61b17fb
Mferik <mfatiherikk@gmail.com>
2023-07-06 13:10:26 +0300

    GetProductDetails metodu ProductService'e eklendi ve ProductManagerde implemente edildi. ConsoleUI da ProductName ve CategoryName yazılması test edildi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             //Data Transformation Objest - DTO
-            //ProductTest();
+            ProductTest();
             //CategoryTest();
         }
 
@@ -25,9 +25,9 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(15, 100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
         }
     }
