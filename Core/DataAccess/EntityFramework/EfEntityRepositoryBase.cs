2f6db5c7116a72877d3b133513344851d7878ab7
Mferik <mfatiherikk@gmail.com>
2023-07-06 11:41:35 +0300

    EfEntityRepositoryBase'ye generic constraintler yazıldı

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext>
+        where TEntity : class,IEntity,new()
+        where TContext : DbContext,new()
+    {
+
+    }
+}
