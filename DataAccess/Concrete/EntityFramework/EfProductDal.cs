3625bab50a176a0e7f3eb43357f7650f9bcbbf43
Mferik <mfatiherikk@gmail.com>
2023-07-06 11:53:09 +0300

    EfEntityRepositoryBase, Generic haliyle yazıldı

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -14,52 +14,6 @@ namespace DataAccess.Concrete.EntityFramework
     public class EfProductDal : IProductDAL
     {
 
-        public void Add(Product entity)
-        {
-            //IDisposable pattern implementation of c#
-            using (NorthwindContext context = new NorthwindContext()) //using içerisine yazdığın nesneler using bitti an Garbage Collector gelir
-            {
-                var addedEntity = context.Entry(entity); //referansı yakala
-                addedEntity.State = EntityState.Added; //eklenecek nesne
-                context.SaveChanges(); //ekle ve kaydet
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext()) //using içerisine yazdığın nesneler using bitti an Garbage Collector gelir
-            {
-                var deletedEntity = context.Entry(entity); //referansı yakala
-                deletedEntity.State = EntityState.Deleted; //silinecek nesne
-                context.SaveChanges(); //ekle ve kaydet
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>>? filter = null)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList();
-
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext()) //using içerisine yazdığın nesneler using bitti an Garbage Collector gelir
-            {
-                var updatedEntity = context.Entry(entity); //referansı yakala
-                updatedEntity.State = EntityState.Modified; //güncellenecek nesne
-                context.SaveChanges(); //ekle ve kaydet
-            }
-        }
+       
     }
 }
