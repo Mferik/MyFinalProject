c195b31ac932f7368ccca2d35d825674b27cd2c4
Mferik <mfatiherikk@gmail.com>
2023-07-10 10:29:34 +0300

    Autofac ile IoC WebAPI katmanında oluşturduk ve Autofac Konfigürasyonları yazıldı Program.cs'e

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolves.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -14,15 +17,19 @@ namespace WebAPI
             // Add services to the container.
             //Autofac,Ninject,Castwindsor,StructureMap,Lightinject,DryInject --> IoC Container
             builder.Services.AddControllers();
-            builder.Services.AddSingleton<IProductService,ProductManager>(); // Eðer ilk tipte bir baðýmlýlýk gösterirsen ikinci parametre karþýlýðýdýr. Arka planda bizim için new'liyor //Tüm bellekte tek bir nesne üretiyor // Ýçinde data tutmuyorsan kullanýyorsun
-            builder.Services.AddSingleton<IProductDAL,EfProductDal>(); //ProductManager IProductDAL'a baðýmlý oldugu için onun için de bir bellekte yer açtýk
-
+            // builder.Services.AddSingleton<IProductService,ProductManager>(); // Eðer ilk tipte bir baðýmlýlýk gösterirsen ikinci parametre karþýlýðýdýr. Arka planda bizim için new'liyor //Tüm bellekte tek bir nesne üretiyor // Ýçinde data tutmuyorsan kullanýyorsun
+            // builder.Services.AddSingleton<IProductDAL,EfProductDal>(); //ProductManager IProductDAL'a baðýmlý oldugu için onun için de bir bellekte yer açtýk
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+            builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+            {
+                builder.RegisterModule(new AutofacBusinessModule());
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            
             var app = builder.Build();
-
+          
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
