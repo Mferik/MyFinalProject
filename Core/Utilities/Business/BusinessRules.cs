6f3d12a73f4f44408a128daca7437d73f43bc1f9
Mferik <mfatiherikk@gmail.com>
2023-07-11 15:35:07 +0300

    BusinessRules iş motorunun içeriği eklendi

--- Core/Utilities/Business/BusinessRules.cs
+++ Core/Utilities/Business/BusinessRules.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Utilities.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace Core.Utilities.Business
 {
     public class BusinessRules
     {
+        public static IResult Run(params IResult[] logics) //IResult array'inin parametrelerini getirir.
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                {
+                    return logic;
+                }
+            }
+            return null;
+        }
     }
 }
